b99425c6bd686f841723bbd6800852a450ca7a89
Babar <babarsmoke24@gmail.com>
2025-06-15 20:13:53 +0530

    feat : Learnt about UrlHandling

---
+++ 20UrlHandling/go.mod
@@ -0,0 +1,3 @@
+module HandleUrl
+
+go 1.22.2
---
+++ 20UrlHandling/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+const myUrl = "postgres://user:pass@host.com:5432/path?k=v#f"
+
+func main() {
+
+	fmt.Println("Welcome to Handling Url with golang")
+	result, err := url.Parse(myUrl)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result.Scheme)
+	fmt.Println(result.User)
+	fmt.Println(result.User.Password())
+	partofUrl := &url.URL{
+		Scheme: "https",
+		Host:   "google.com",
+	}
+	MakeUrl := partofUrl.String()
+	fmt.Println("Url : ", MakeUrl)
+	// fmt.Println(result.Port())
+	// fmt.Println(result.Path)
+	// fmt.Println(result.RawQuery)
+}
