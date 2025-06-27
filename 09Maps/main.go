39134f42e5a313f31cd0b1227580a14645ff7b14
Babar <babarsmoke24@gmail.com>
2025-06-27 18:13:49 +0530

    feat : Learnt about MAPS HEHE

--- 09Maps/main.go
+++ 09Maps/main.go
@@ -6,6 +6,7 @@ func main() {
 	fmt.Println("Maps in Golang")
 
 	languages := make(map[string]string)
+	country := make(map[string]bool)
 	languages["JS"] = "Javascript"
 	languages["Py"] = "Python"
 	languages["ts"] = "Typescript"
@@ -17,4 +18,18 @@ func main() {
 	// delete(languages, "P")
 	//FIX : Why does it not throw error ? and also find loop
 	fmt.Println("List of Languages : ", languages)
+	country["IND"] = true
+	country["PAK"] = false
+	for k, _ := range country {
+		// country[k] = vis
+
+		if !country[k] {
+			country[k] = true
+			fmt.Println("Unvisited Has been Visited")
+			fmt.Println("Unvisted Country was : ", k)
+		} else {
+			fmt.Println("Already Visited")
+		}
+	}
+
 }
