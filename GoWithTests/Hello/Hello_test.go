609b83424c8cf946fb5d661906d9d6e2baa921b3
Babar <babarsmoke24@gmail.com>
2025-07-05 20:57:38 +0530

    Refactor  : Adding tests for different Languages

--- GoWithTests/Hello/Hello_test.go
+++ GoWithTests/Hello/Hello_test.go
@@ -3,10 +3,46 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Goku")
-	want := "Hello, Goku"
+	// t.Run("Saying hello to People", func(t *testing.T) {
+	//
+	// 	got := Hello("Goku")
+	// 	want := "Hello, Goku"
+	// 	assertCorrectMessage(t, got, want)
+	// })
+	// t.Run("Saying hello to nobody", func(t *testing.T) {
+	// 	got := Hello("")
+	// 	want := Hello("Legend")
+	// 	assertCorrectMessage(t, got, want)
+	//
+	// })
+	t.Run("in Spanish ", func(t *testing.T) {
+		got := Hello("Elodie ", "Spanish")
+		want := "Elodie Spanish"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French ", func(t *testing.T) {
+		got := Hello("Bonjour ", "French")
+		want := "Bonjour French"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+func assertCorrectMessage(t testing.TB, got, want string) {
+	//NOTE : If you same type like want string, got string ,-> You can give want,got string
+	//NOTE : This is a helper func that is satified by both *T and *B
+	t.Helper()
 	if got != want {
 		t.Errorf("Got %q and want %q", want, got)
 	}
 
 }
+
+/*
+//NOTE :
+1) Write a test
+2) Make the compiler pass
+3) Run the test and see if it fails and check the error message
+4) Write enough test to make it pass
+5) Refactor
+
+*/
--- GoWithTests/Hello/main.go
+++ GoWithTests/Hello/main.go
@@ -2,12 +2,31 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+// NOTE : Declaring multiple constants together
+const (
+	EnglishHelloPrefix = "Hello, "
+	SpanishHelloPrefix = "Elodie "
+	FrenchHelloPrefix  = "Bonjour "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "Super"
+	}
+
+	if language == "Spanish" {
+		return SpanishHelloPrefix + language
+	}
+	if language == "French" {
+		return FrenchHelloPrefix + language
+	}
+	return EnglishHelloPrefix + name
 
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", "Spanish"))
 
 }
+
+//TODO : Last Refactor
