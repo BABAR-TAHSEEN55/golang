854864c6564878b11f0639c8b0272722243be123
Babar <babarsmoke24@gmail.com>
2025-07-06 14:41:31 +0530

    Refactor : Testing

--- GoWithTests/Arrays/arrays_test.go
+++ GoWithTests/Arrays/arrays_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	// t.Run("Coljection of 5 numbers", func(t *testing.T) {
@@ -22,6 +25,38 @@ func TestSum(t *testing.T) {
 		if got != want {
 			//NOTE  : For print -> Default specifier for Arrays : %v
 			t.Errorf("Got %d and want %d numbers %v", got, want, numbers)
+		}
+	})
+	t.Run("Slices additon of numbers", func(t *testing.T) {
+		got := SumAll([]int{1, 3}, []int{3, 4})
+		want := []int{4, 7}
+		//NOTE : Can't use != with slices
+		// if got != want {
+		if reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v and want %v", got, want)
+
+		}
+	})
+}
+func TestSumAllTails(t *testing.T) {
+
+	t.Run("Counting tails", func(t *testing.T) {
+		got := SumAllTails([]int{1, 3}, []int{3, 4})
+		want := []int{3, 4}
+		//NOTE : Can't use != with slices
+		// if got != want {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v and want %v", got, want)
+
+		}
+	})
+	t.Run("Counting tails", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		// if got != want {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v and want %v", got, want)
+
 		}
 	})
 
--- GoWithTests/Arrays/main.go
+++ GoWithTests/Arrays/main.go
@@ -7,3 +7,32 @@ func Sum(number []int) int {
 	}
 	return sum
 }
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumber := len(numbersToSum)
+	sums := make([]int, lengthOfNumber)
+	for i, num := range numbersToSum {
+		sums[i] = Sum(num)
+	}
+	return sums
+}
+
+//NOTE : go test -cover
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, num := range numbersToSum {
+		if len(num) == 0 {
+			sums = append(sums, 0)
+
+		} else {
+
+			tail := num[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
+
+//TODO : More example and 1 last refactor
+//TODO : Compile the failing tes 
