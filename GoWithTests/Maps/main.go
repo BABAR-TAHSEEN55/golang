17d5bec1142e8e5fcc22fe43d33eafa438ce44f2
Babar <babarsmoke24@gmail.com>
2025-07-16 12:43:59 +0530

    feat : Learnt about Maps in depths and also on creating error wrappers

--- GoWithTests/Maps/main.go
+++ GoWithTests/Maps/main.go
@@ -2,7 +2,11 @@ package main
 
 import "errors"
 
-var ErrorNotFound = errors.New("Could not find the meaning of this word")
+var (
+	ErrorNotFound          = errors.New("Could not find the meaning of this word")
+	ErrorWordExists        = errors.New("Word already exists")
+	ErrorWordDoesNotExists = errors.New("Word doesn't exists")
+)
 
 type Dictionary map[string]string
 
@@ -13,10 +17,47 @@ func (d Dictionary) Search(word string) (string, error) {
 	}
 	return value, nil
 }
-func (d Dictionary) Add(word string, defination string) {
-	d[word] = defination
-
+func (d Dictionary) Add(word string, defination string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrorNotFound:
+		d[word] = defination
+	case nil:
+		return ErrorWordExists
+	default:
+		return err
+	}
+	return nil
 }
+func (d Dictionary) Update(word, newDefination string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrorNotFound:
+		return ErrorWordDoesNotExists
+	case nil:
+		d[word] = newDefination
+	default:
+		return err
+
+	}
+	return nil
+}
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrorNotFound:
+		return ErrorWordDoesNotExists
+	case nil:
+		delete(d, word)
+	default:
+		return err
+	}
+	return nil
+}
+
+// NOTE : Return type as error means i'll return an error if something goes wrong and nil if everything goes right both belongs to error interface
 func main() {
 
 }
+
+//TODO : Create a Bank Stimulation -> Complete End to End along with Tests
--- GoWithTests/Maps/maps_test.go
+++ GoWithTests/Maps/maps_test.go
@@ -13,21 +13,28 @@ func TestMaps(t *testing.T) {
 		assertStrings(t, got, want)
 	})
 	t.Run("UnKnown Word", func(t *testing.T) {
-		_, got := dictionary.Search("Unknown")
-		if got == nil {
+		_, err := dictionary.Search("Unknown")
+		if err == nil {
 			t.Fatal("Error in Unknown error")
 		}
-		assertError(t, got, ErrorNotFound)
+		assertError(t, err, ErrorNotFound)
 	})
 	t.Run("Add a word", func(t *testing.T) {
 		dictionary := Dictionary{}
-		dictionary.Add("test", "This is the added test")
-		want := "This is the added test"
-		got, err := dictionary.Search("test")
-		if err != nil {
-			t.Fatal("Should find added word", err)
-		}
-		assertStrings(t, got, want)
+		word := "test"
+		defination := "This is the added test"
+		dictionary.Add(word, defination)
+		// assertStrings(t, got, want)
+		assertDefination(t, dictionary, word, defination)
+
+	})
+	t.Run("Existing word", func(t *testing.T) {
+		word := "test"
+		defination := "This is just a test"
+		dictionary := Dictionary{word: defination}
+		err := dictionary.Add(word, "new test")
+		assertDefination(t, dictionary, word, defination)
+		assertError(t, err, ErrorWordExists)
 
 	})
 
@@ -43,5 +50,44 @@ func assertError(t testing.TB, want, got error) {
 	if got != want {
 		t.Fatal("expected an error")
 	}
+
+}
+func assertDefination(t testing.TB, dictionary Dictionary, word, defination string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	// want := "This is the added test"
+	if err != nil {
+		t.Fatal("Should find added word", err)
+	}
+	assertStrings(t, got, defination)
+	//NOTE : Here defination becomes your want
+}
+func TestUpdate(t *testing.T) {
+	t.Run("Existing Word", func(t *testing.T) {
+
+		word := "test"
+		defination := "this is just a test"
+		Dictionary := Dictionary{word: defination}
+		newDefination := "This is the updated Test"
+		Dictionary.Update(word, newDefination)
+		assertDefination(t, Dictionary, word, newDefination)
+	})
+	t.Run("New Word", func(t *testing.T) {
+		word := "New Word"
+		defination := "This is the new word"
+		Dictionary := Dictionary{}
+		err := Dictionary.Update(word, defination)
+		assertError(t, err, ErrorWordDoesNotExists)
+	})
+
+}
+func TestDelete(t *testing.T) {
+	word := "New Word"
+	Dictionary := Dictionary{word: "Test defination"}
+	err := Dictionary.Delete(word)
+	assertError(t, err, nil)
+	_, err = Dictionary.Search(word) // No need to use := as it has been already declared but what about scope?
+	assertError(t, err, ErrorNotFound)
 
 }
